00fe03c9f4aa33c8da21d3b688a3912b96f839da
Richard Challis <richard.challis@sanger.ac.uk>
2022-10-11 17:48:36 +0100

    set midpoint_proportion for window and busco features

--- scripts/4-process_btk_directory.sh
+++ scripts/4-process_btk_directory.sh
@@ -16,9 +16,28 @@ for DIR in $(ls -d ${BTK_DIR}/*/); do
   TOP_LINEAGE=$(echo "$LINEAGES" | head -n 1)
   LIBRARY=$(echo "$LIBRARIES" | head -n 1)
 
+  # turn chunks into windows
+  btk pipeline window-stats \
+      --window 10000 \
+      --window 100000 \
+      --window 1000000 \
+      --window 0.02 \
+      --window 0.05 \
+      --window 0.1 \
+      --min-window-length 1000 \
+      --min-window-count 5 \
+      --in "${DIR}/${BTK_ID}.chunk_stats.tsv.gz" \
+      --out "${BTK_ID}.window_stats.tsv"
+  
+  for FILE in $(ls ${BTK_ID}.window_stats.*.tsv); do
+    gzip -c $FILE > "sources/features/${FILE}.gz"
+    rm $FILE
+  done
+
+  cp "${DIR}/${BTK_ID}.window_stats.tsv.gz" "sources/features/${BTK_ID}.window_stats.tsv.gz"
+
 # write window_stats YAML
 
-cp "${DIR}/${BTK_ID}.window_stats.tsv.gz" "sources/features/${BTK_ID}.window_stats.tsv.gz"
 echo "file:
   format: tsv
   header: true
@@ -75,6 +94,74 @@ if [ ! -z "$LIBRARY" ]; then
     header: ${LIBRARY}_cov
     function: \"{} + 0.01\"" >> "sources/features/FILE_${BTK_ID}.window_stats.types.yaml"
 fi
+
+for FILE in $(ls sources/features/${BTK_ID}.window_stats.*.tsv.gz); do
+    WINDOW_FILE=$(basename ${FILE})
+    WINDOW=$(perl -e 'my @parts = split(m[(?:window_stats\.|\.tsv\.gz)], "'${WINDOW_FILE}'"); print "$parts[1]";')
+
+echo "file:
+  format: tsv
+  header: true
+  name: ${WINDOW_FILE}
+  needs:
+    - ATTR_feature.types.yaml
+    - ATTR_window_stats.types.yaml
+    - FILE_${BTK_ID}.window_stats.types.yaml
+  source: BlobToolKit
+  source_url: https://blobtoolkit.genomehubs.org/view/dataset/$BTK_ID
+analysis:
+  analysis_id: assembly-${ACCESSION}-${WINDOW}
+  assembly_id: ${ACCESSION}
+  taxon_id: \"${TAXID}\"
+  description: Public assembly ${ACCESSION} ${WINDOW} windows
+  name: Assembly-${WINDOW}
+  title: Public assembly ${ACCESSION} ${WINDOW}
+features:
+  assembly_id: ${ACCESSION}
+  feature_id:
+    header: sequence
+    template: \"{}:{start}-{end}:window\"
+  primary_type: window-${WINDOW}
+taxonomy:
+  taxon_id: \"$TAXID\"
+attributes:
+  sequence_id:
+    header: sequence
+  feature_type:
+    default:
+      - window-${WINDOW}
+      - window
+  start:
+    header: start
+    function: \"{} + 1\"
+  end:
+    header: end
+  strand: 1
+  length:
+    header: start
+    function: \"{end} - {}\"
+  seq_proportion:
+    header: sequence
+    function: \"{length} / {.length}\"
+  midpoint:
+    header: end
+    function: \"{length} / 2 + {start}\"
+  midpoint_proportion:
+    header: sequence
+    function: \"{midpoint} / {.length}\"
+  gc:
+    header: gc
+  masked:
+    header: masked
+  busco_count:
+    header: ${TOP_LINEAGE}_count" > "sources/features/FILE_${BTK_ID}.window_stats.${WINDOW}.types.yaml"
+if [ ! -z "$LIBRARY" ]; then
+  echo "  coverage:
+    header: ${LIBRARY}_cov
+    function: \"{} + 0.01\"" >> "sources/features/FILE_${BTK_ID}.window_stats.${WINDOW}.types.yaml"
+fi
+
+done
 # write busco YAMLs
 while read LINEAGE; do
 
@@ -101,13 +188,19 @@ features:
   assembly_id: ${ACCESSION}
   feature_id:
     index: 2
-    template: \"{}:{start}-{end}\"
+    separator:
+      - \":\"
+    limit: 1
+    template: \"{}:{start}-{end}:{busco_gene}\"
   primary_type: busco-gene
 taxonomy:
   taxon_id: \"$TAXID\"
 attributes:
   sequence_id:
     index: 2
+    separator:
+      - \":\"
+    limit: 1
   feature_type:
     default:
       - busco-gene
@@ -128,6 +221,9 @@ attributes:
     function: \"{length} / 2 + {}\"
   midpoint_proportion:
     index: 2
+    separator:
+      - \":\"
+    limit: 1
     function: \"{midpoint} / {.length}\"
   busco_gene:
     index: 0
--- scripts/5-import_features.sh
+++ scripts/5-import_features.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
 # Run genomehubs import feature-dir
+curl -X DELETE "http://localhost:9200/taxon-*"
+curl -X DELETE "http://localhost:9200/a*"
+curl -X DELETE "http://localhost:9200/f*"
+
+genomehubs init \
+    --config-file sources/lepbase.yaml \
+    --taxonomy-source ncbi \
+    --taxon-preload \
+    --restore-indices &&
+
+genomehubs index \
+    --es-host http://localhost:9200 \
+    --taxonomy-source ncbi \
+    --config-file sources/lepbase.yaml \
+    --assembly-dir sources/assembly-data
 
 genomehubs index \
     --es-host http://localhost:9200 \
